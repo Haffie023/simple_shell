0d93ce85c16f9bbf10ecb547e28858361d1ec9d1
Sgechuki <sgechuki@outlook.com>
2023-07-15 12:46:59 +0300

    Co-authored-by: Hafsa Muchoki <joyce.muchoki@gmail.com>

---
+++ simple_shell.h
@@ -0,0 +1,14 @@
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
+
+int main (void);
+char *prompt(void);
+char *getcmd(void);
+/**
+* parse - strtok, malloc, realloc
+* fork()
+* execve
+**/
+int _putchar(char c);
+
+#endif

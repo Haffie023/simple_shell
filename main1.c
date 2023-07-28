c923c20a49769f5d3d03102d07d34f243207bbe1
Haffie023 <joyce.muchoki@gmail.com>
2023-07-28 17:54:05 +0300

    updated

---
+++ main1.c
@@ -0,0 +1,45 @@
+#include "simple_shell.h"
+
+int main(int ac __attribute__((unused)), char **av, char **env)
+{
+	char *buf = NULL;
+	char **segments = NULL;
+	char *fullpath = NULL;
+	size_t n = 0;
+	int imode = isatty(STDIN_FILENO);
+	int flag = 1;
+
+	while (imode)
+	{
+		prompt();
+		if (getline(&buf, &n, stdin) == -1) {
+			perror("getline");
+			break;
+		}
+		if (buf[0] == '\n')
+		{
+			continue;
+		}
+		dfree(segments); // Free previous segments
+		segments = tokenize(buf);
+		if (builtin(segments) == 0)
+			continue;
+		dfree(fullpath); // Free previous fullpath
+		fullpath = getfullpath(segments);
+		if (fullpath == NULL)
+		{
+			perror(av[0]);
+			dfree(segments);
+			continue;
+		}
+		runlcmd(fullpath, segments, env);
+		if (flag)
+			sfree(fullpath);
+	}
+
+	free(buf);
+	dfree(segments);
+	dfree(fullpath);
+
+	return (0);
+}
